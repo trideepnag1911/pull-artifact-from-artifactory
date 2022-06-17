5ee9940f37306466485b1b6b9e16e28d7f7b09ed
trideepnag1911 <trideepnag@gmail.com>
2022-06-17 09:56:57 -0700

    updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-curl -sSf -u "$USER_NAME":"$PASSWORD" -O "$FILE"
+curl -sSf -u "$USER_NAME":"$PASSWORD" -o "$TARGET" "$FILE"
 if [ $? -eq 0 ]
 then
 echo "Completed"
